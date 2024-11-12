4f5cd727d6abc522d8ad1d4d7f1e88fc09b9a9ac
mahjs <mahjsdev@gmail.com>
2024-11-12 19:14:10 +0330

    add components and it routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/LoginPage.vue'
+import Register from '@/views/RegisterPage.vue'
+import CreateArticle from '@/views/CreateArticle.vue'
+import EditArticle from '@/views/EditArticle.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,26 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+    {
+      path: '/new-article',
+      name: 'new-article',
+      component: CreateArticle,
+    },
+    {
+      path: '/edit-article',
+      name: 'edit-article',
+      component: EditArticle,
     },
   ],
 })
---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Create Article</div>
+</template>
