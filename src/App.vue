0aa5ec7b1f80d028e237af899339c67efc284f1b
mahjs <mahjsdev@gmail.com>
2024-11-12 19:02:34 +0330

    initializing project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+    <router-view />
+  </main>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+}
+</style>
